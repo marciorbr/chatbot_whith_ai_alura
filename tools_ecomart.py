e2d552739d4ca9f46bc50fce2190bc9565169c7c
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2024-02-23 14:15:28 -0400

    adicionando tool function call para validar cupons

--- tools_ecomart.py
+++ tools_ecomart.py
@@ -13,4 +13,27 @@ cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4-1106-preview"
 
 minhas_tools = [
-    {"type": "retrieval"},]
+        {"type": "retrieval"},
+        {
+            "type": "function",
+                        "function": {
+                        "name": "validar_codigo_promocional",
+                        "description": "Valide um código promocional com base nas diretrizes de Descontos e Promoções da empresa",
+                        "parameters": {
+                                "type": "object",
+                                "properties": {
+                                        "codigo": {
+                                                "type": "string",
+                                                "description": "O código promocional, no formato, CUPOM_XX. Por exemplo: CUPOM_ECO",
+                                        },
+                                        "validade": {
+                                                "type": "string",
+                                                "description": f"A validade do cupom, caso seja válido e esteja associado as políticas. No formato DD/MM/YYYY.",
+                                        },
+                                },
+                                "required": ["codigo", "validade"],
+                        }
+                }
+        }
+
+]
